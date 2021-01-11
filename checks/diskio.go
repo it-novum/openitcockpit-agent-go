e9efa6507b0ab481af5108cc28ccc2bd23c628fc
nook24 <info@nook24.eu>
2021-01-11 16:11:32 +0100

    Add comments to disk io struct

--- checks/diskio.go
+++ checks/diskio.go
@@ -15,24 +15,24 @@ func (c *CheckDiskIo) Name() string {
 }
 
 type resultDiskIo struct {
-	ReadBytes    uint64
-	WriteBytes   uint64
-	ReadIops     uint64
-	WriteIops    uint64
-	TotalIops    uint64
-	ReadCount    uint64
-	WriteCount   uint64
-	IoTime       uint64
-	ReadAvgWait  float64
-	ReadTime     uint64
-	ReadAvgSize  float64
-	WriteAvgWait float64
-	WriteAvgSize float64
-	WriteTime    uint64
-	TotalAvgWait float64
-	LoadPercent  int64
-	Timestamp    int64
-	Device       string
+	ReadBytes    uint64  // Number of bytes read from disk since last evaluation
+	WriteBytes   uint64  // Number of bytes written to disk since last evaluation
+	ReadIops     uint64  // Number of read iops since last evaluation
+	WriteIops    uint64  // Number of write iops since last evaluation
+	TotalIops    uint64  // Total number of write iops since last evaluation
+	ReadCount    uint64  // Number of read iops since last evaluation (same as ReadIops)
+	WriteCount   uint64  // Number of write iops since last evaluation (same as WriteIops)
+	IoTime       uint64  // Time spent doing actual I/Os (in milliseconds) (busy_time in python psutil)
+	ReadAvgWait  float64 // Average io_wait for read iops in milliseconds
+	ReadTime     uint64  // Number of io_wait for read iops in milliseconds
+	ReadAvgSize  float64 // Average request size of reads in bytes
+	WriteAvgWait float64 // Average io_wait for write iops in milliseconds
+	WriteAvgSize float64 // Average request size of writes in bytes
+	WriteTime    uint64  // Number of io_wait for write iops in milliseconds
+	TotalAvgWait float64 // Total io_wait in milliseconds
+	LoadPercent  int64   // Disk load as percentage
+	Timestamp    int64   // Timestamp of the last check evaluation
+	Device       string  // Name of the disk
 }
 
 // Configure the command or return false if the command was disabled
