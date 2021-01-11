599c5834cf61aba6e60986b2a00b858da075992b
nook24 <info@nook24.eu>
2021-01-11 16:15:50 +0100

    Add comments to agent struct

--- checks/agent.go
+++ checks/agent.go
@@ -18,15 +18,15 @@ func (c *CheckAgent) Name() string {
 }
 
 type resultAgent struct {
-	LastUpdated          string `json:"last_updated"`
-	LastUpdatedTimestamp int64  `json:"last_updated_timestamp"`
-	System               string `json:"system"`
-	SystemUptime         uint64 `json:"system_uptime"`
-	KernelVersion        string `json:"kernel_version"`
-	MacVersion           string `json:"mac_version"`
-	Family               string `json:"family"`
-	AgentVersion         string `json:"agent_version"`
-	TemperatureUnit      string `json:"temperature_unit"`
+	LastUpdated          string `json:"last_updated"`           // e.g.: 2021-01-11 15:58:35.987952 +0100 CET m=+19.945268128
+	LastUpdatedTimestamp int64  `json:"last_updated_timestamp"` // w.g.: 1610377115
+	System               string `json:"system"`                 // darwin | linux | windows
+	SystemUptime         uint64 `json:"system_uptime"`          // System uptime in seconds
+	KernelVersion        string `json:"kernel_version"`         // e.g.: 19.6.0
+	MacVersion           string `json:"mac_version"`            // e.g.: 10.15.7
+	Family               string `json:"family"`                 // Standalone Workstation | Server
+	AgentVersion         string `json:"agent_version"`          // e.g. 3.0.0
+	TemperatureUnit      string `json:"temperature_unit"`       // C (hardcoded)
 }
 
 // Run the actual check
@@ -44,14 +44,14 @@ func (c *CheckAgent) Run(ctx context.Context) (interface{}, error) {
 
 	now := time.Now()
 	return &resultAgent{
-		LastUpdated:          now.String(),        // e.g.: 2021-01-11 15:58:35.987952 +0100 CET m=+19.945268128
-		LastUpdatedTimestamp: now.Unix(),          // w.g.: 1610377115
-		System:               platfrom,            // darwin | linux | windows
-		SystemUptime:         uptime,              // System uptime in seconds
-		KernelVersion:        kernel,              // e.g.: 19.6.0
-		MacVersion:           pver,                // e.g.: 10.15.7
-		Family:               family,              // Standalone Workstation | Server
-		AgentVersion:         config.AgentVersion, // e.g. 3.0.0
+		LastUpdated:          now.String(),
+		LastUpdatedTimestamp: now.Unix(),
+		System:               platfrom,
+		SystemUptime:         uptime,
+		KernelVersion:        kernel,
+		MacVersion:           pver,
+		Family:               family,
+		AgentVersion:         config.AgentVersion,
 		TemperatureUnit:      "C",
 	}, nil
 }
