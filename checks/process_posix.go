edc1ef878801e4139eab8c50bdf49dae5f091636
nook24 <info@nook24.eu>
2021-03-02 19:56:10 +0100

    Implement @ibering s perfect header parser

--- checks/process_posix.go
+++ checks/process_posix.go
@@ -5,6 +5,7 @@ package checks
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -42,63 +43,127 @@ func (c *CheckProcess) Run(ctx context.Context) (interface{}, error) {
 	}
 
 	lines := strings.Split(result.Stdout, "\n")
+	headerLine := lines[0]
+
 	if len(lines) > 0 {
 		//Remove first line
 		lines = lines[1:]
 	}
 
-	var processes [][]string
-	for _, l := range lines {
-		var lr []string
-		for _, r := range strings.Split(l, " ") {
-			if r == "" {
-				continue
-			}
-			lr = append(lr, strings.TrimSpace(r))
+	var positions = make(map[string]map[string]int)
+
+	re := regexp.MustCompile(`\S+`)
+	submatchall := re.FindAllString(headerLine, -1)
+	start := 0
+	end := 0
+	for i := 0; i < len(submatchall); i++ {
+		if i != len(submatchall)-1 {
+			//fmt.Println("Ends with : ",i, strings.Index(headerLine, submatchall[i+1])-1 )
+			end = strings.Index(headerLine, submatchall[i+1]) - 1
+		} else {
+			end = 0
 		}
-		if len(lr) != 0 {
-			processes = append(processes, lr)
+		if i > 0 {
+			start = strings.Index(headerLine, submatchall[i])
 		}
+
+		positions[submatchall[i]] = map[string]int{
+			"start": start,
+			"end":   end,
+		}
+		//fmt.Println(submatchall[i], "Started with : ", start, " and ends with ", end) // Position 0
+		//if end > 0 {
+		//	fmt.Println("part = ", i, string(headerLine[start:end]))
+		//} else {
+		//	fmt.Println("part = ", i, string(headerLine[start:]))
+		//}
 	}
+
+	//fmt.Println(positions)
 
 	processResults := make([]*resultProcess, 0, len(lines))
 
-	for _, p := range processes {
-		//   0     1     2    3    4                5  6      7        8      9   10                 11
-		// PID  PPID  %CPU %MEM USER             STAT NI    RSS      VSZ PAGEIN COMM             COMMAND
-		pid, _ := strconv.ParseInt(string(p[0]), 10, 32)
-		ppid, _ := strconv.ParseInt(string(p[1]), 10, 32)
-		cpu, _ := strconv.ParseFloat(string(p[2]), 64)
-		mem, _ := strconv.ParseFloat(string(p[3]), 64)
-		user := string(p[4])
-		state := string(p[5])
-		nice, _ := strconv.Atoi(string(p[6]))
-		rss, _ := strconv.ParseInt(string(p[7]), 10, 64)
-		vsz, _ := strconv.ParseInt(string(p[8]), 10, 64)
-		pagein, _ := strconv.ParseInt(string(p[1]), 10, 64)
-		bin := string(p[10])
-		cmdline := string(p[11])
+	fields := []string{"PID", "PPID", "%CPU", "%MEM", "USER", "STAT", "NI", "RSS", "VSZ", "PAGEIN", "COMM", "COMMAND"}
 
-		s := getStatusName(state[0:1])
-		var status []string
-		status = append(status, s)
-
-		result := &resultProcess{
-			Pid:           int32(pid),
-			Ppid:          int32(ppid),
-			Username:      user,
-			Name:          bin,
-			CPUPercent:    cpu,
-			MemoryPercent: float32(mem),
-			Cmdline:       cmdline,
-			Status:        status,
-			Exe:           bin,
-			Nice:          int32(nice),
-			NumFds:        0,
+	for _, l := range lines {
+		if l == "" {
+			continue
 		}
-		result.Memory.RSS = uint64(rss)
-		result.Memory.VMS = uint64(vsz)
-		result.Memory.Swap = uint64(pagein)
+
+		result := &resultProcess{}
+		for _, field := range fields {
+			var value string
+			if field != "COMMAND" {
+				start := positions[field]["start"]
+				end := positions[field]["end"]
+				value = strings.TrimSpace(l[start:end])
+			} else {
+				// Comand ist the last element in the string, so get all remaining characters
+				start := positions[field]["start"]
+				value = strings.TrimSpace(l[start:])
+			}
+
+			if field == "PID" {
+				pid, _ := strconv.ParseInt(value, 10, 32)
+				result.Pid = int32(pid)
+			}
+
+			if field == "PPID" {
+				ppid, _ := strconv.ParseInt(value, 10, 32)
+				result.Ppid = int32(ppid)
+			}
+
+			if field == "%CPU" {
+				cpu, _ := strconv.ParseFloat(value, 64)
+				result.CPUPercent = cpu
+			}
+
+			if field == "%MEM" {
+				mem, _ := strconv.ParseFloat(value, 64)
+				result.MemoryPercent = float32(mem)
+			}
+
+			if field == "USER" {
+				result.Username = value
+			}
+
+			if field == "STAT" {
+				s := getStatusName(value[0:1])
+				var status []string
+				status = append(status, s)
+
+				result.Status = status
+			}
+
+			if field == "NI" {
+				nice, _ := strconv.Atoi(value)
+				result.Nice = int32(nice)
+			}
+
+			if field == "RSS" {
+				rss, _ := strconv.ParseInt(value, 10, 64)
+				result.Memory.RSS = uint64(rss)
+			}
+
+			if field == "VSZ" {
+				vsz, _ := strconv.ParseInt(value, 10, 64)
+				result.Memory.VMS = uint64(vsz)
+			}
+
+			if field == "PAGEIN" {
+				pagein, _ := strconv.ParseInt(value, 10, 64)
+				result.Memory.Swap = uint64(pagein)
+			}
+
+			if field == "COMM" {
+				result.Exe = value
+			}
+
+			if field == "COMMAND" {
+				result.Cmdline = value
+			}
+
+		}
 
 		processResults = append(processResults, result)
 	}
