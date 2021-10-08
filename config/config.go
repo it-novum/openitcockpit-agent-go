e4d706d9ffd64a91e414eb9fb55c5681b73c0b49
Daniel <nook24@users.noreply.github.com>
2021-10-08 08:40:37 +0200

    Set agent version to 3.0.7

--- config/config.go
+++ config/config.go
@@ -14,7 +14,7 @@ import (
 )
 
 // AgentVersion as the name says
-const AgentVersion = "3.0.6"
+const AgentVersion = "3.0.7"
 
 // CustomCheck are external plugins and scripts which should be executed by the Agent
 type CustomCheck struct {
